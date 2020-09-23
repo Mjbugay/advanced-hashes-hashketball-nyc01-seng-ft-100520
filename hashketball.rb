87c77ab70b2ef2defec57a539ba7e1160db2dfe3
Matt Bugay <mjbugay@yahoo.com>
2020-09-23 17:11:18 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -143,7 +143,8 @@ end
 def shoe_size(name)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[:]
+      if player[:player_name] == name
+        return 
       
       
       
