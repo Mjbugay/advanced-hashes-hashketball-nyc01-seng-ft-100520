a0c9a7e64b4ee2c8dc67a50c0e57a23a179d10a2
Matt Bugay <mjbugay@yahoo.com>
2020-09-23 17:07:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -133,7 +133,8 @@ end
 def num_points_scored(player_search)
   game_hash.each do |team, team_info|
     team_info[:players].each do |player|
-      if player[]
+      if player[:player_name] == player_search
+        return 
     
     
     
